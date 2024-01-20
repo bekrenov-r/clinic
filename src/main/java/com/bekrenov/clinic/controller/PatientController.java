e4ad54359fe8d6733ab7609e5da6f94cfa83b2ca
Roman <bekrenov.roman@gmail.com>
2024-01-20 19:33:41 +0100

    Implement endpoint for fetching user profile; remove unimplemented endpoints in PatientController

--- src/main/java/com/bekrenov/clinic/controller/PatientController.java
+++ src/main/java/com/bekrenov/clinic/controller/PatientController.java
@@ -1,10 +1,13 @@
 package com.bekrenov.clinic.controller;
 
-import com.bekrenov.clinic.model.entity.Patient;
+import com.bekrenov.clinic.dto.response.PatientResponse;
 import com.bekrenov.clinic.service.PatientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/patients")
@@ -13,23 +16,8 @@ public class PatientController {
     private final PatientService patientService;
 
     @GetMapping
-    public ResponseEntity<Patient> getPatientProfile(){
-        return null;
+    @Secured("PATIENT")
+    public ResponseEntity<PatientResponse> getPatientProfile(){
+        return ResponseEntity.ok(patientService.getPatientProfile());
     }
-
-    @PostMapping("/register")
-    public ResponseEntity<Patient> registerPatient(){
-        return null;
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Patient> updatePatient(@PathVariable Long id){
-        return null;
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletePatient(@PathVariable Long id){
-        return null;
-    }
-
 }
--- src/main/java/com/bekrenov/clinic/service/PatientService.java
+++ src/main/java/com/bekrenov/clinic/service/PatientService.java
@@ -1,9 +1,22 @@
 package com.bekrenov.clinic.service;
 
+import com.bekrenov.clinic.dto.mapper.PatientMapper;
+import com.bekrenov.clinic.dto.response.PatientResponse;
+import com.bekrenov.clinic.model.entity.Patient;
+import com.bekrenov.clinic.repository.PatientRepository;
+import com.bekrenov.clinic.util.CurrentUserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PatientService {
+    private final PatientRepository patientRepository;
+    private final CurrentUserUtil currentUserUtil;
+    private final PatientMapper patientMapper;
+
+    public PatientResponse getPatientProfile() {
+        Patient patient = patientRepository.findByEmail(currentUserUtil.getCurrentUser().getUsername());
+        return patientMapper.entityToResponse(patient);
+    }
 }
